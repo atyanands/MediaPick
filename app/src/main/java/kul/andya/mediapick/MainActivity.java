326a6f5bf0859c55d13d93861ecb065026ff8be2
Kul Andya <atyanands@gmail.com>
2017-11-22 21:03:13 +0530

    yoo

--- app/src/main/java/kul/andya/mediapick/MainActivity.java
+++ app/src/main/java/kul/andya/mediapick/MainActivity.java
@@ -1,5 +1,6 @@
 package kul.andya.mediapick;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +9,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import kul.andya.media.Gallery;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,8 +25,9 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(MainActivity.this,Gallery.class);
+                intent.putExtra("title","zing");
+               startActivityForResult(intent,1);
             }
         });
     }
